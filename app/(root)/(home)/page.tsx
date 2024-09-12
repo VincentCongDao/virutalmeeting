5231d9b790c40c9d4acc622b6edcbfd2baa290b8
VincentCongDao <congdanhdao95@gmail.com>
2024-09-11 23:16:52 -0700

    Successfully adding modal into the card

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -16,7 +16,7 @@ const Home = () => {
     return (
         <section className="flex size-full  flex-col gap-10 text-white">
             <div className="h-[300px] w-fulll rounded-[20px] bg-hero bg-cover">
-                <div className="flex h-full  flex-col justify-between max=md:px-5 max-md:py-8 lg:p-11">
+                <div className="flex h-full  flex-col justify-between px-2 max-md:px-5 max-md:py-8 lg:p-11">
                     <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">Upcoming Meeting: 12:30 PM</h2>
                     <div className="flex flex-col gap-2">
                         <h1 className="text-4xl font-extrabold lg:text-7xl">{formatTime}</h1>
--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,8 +1,49 @@
-const MeetingModal = () => {
+import {
+    Dialog,
+    DialogContent,
+} from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+
+interface MeetingModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    title: string;
+    className?: string;
+    buttonText?: string;
+    handleClick?: () => void;
+    image?: string;
+    buttonIcon?: string;
+    children?: React.ReactNode;
+}
+const MeetingModal = ({ isOpen, onClose, title, className, buttonText, handleClick, image, buttonIcon }: MeetingModalProps) => {
     return (
         <div>
-            Meeting Modal
-        </div>
+            <Dialog open={isOpen} onOpenChange={onClose}>
+                <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+                    <div className="flex flex-col gap-6">
+                        {
+                            image &&
+                            (
+                                <div>
+                                    <Image src={image} alt={image} width={72} height={72} />
+                                </div>
+                            )
+                        }
+                        <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>{title}</h1>
+                        <Button className={`bg-pink-1 text-black focus-visible:ring-0 focus-visible:ring-offset-0`} onClick={handleClick}>
+                            {buttonIcon &&
+                                (
+                                    <Image src={buttonIcon} alt={buttonIcon} width={24} height={24} />
+                                )}
+                            {buttonText || "Schedule Meeting"}
+                        </Button>
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </div >
     );
 }
 
